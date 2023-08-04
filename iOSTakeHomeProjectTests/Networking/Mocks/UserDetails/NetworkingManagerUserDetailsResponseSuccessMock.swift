770cda5aee686f73838cb8bbcd8328c164dede49
MrJeris <mrjeris@yandex.ru>
2023-08-04 20:14:08 +0300

    Integration test the Detail View Model

---
+++ iOSTakeHomeProjectTests/Networking/Mocks/UserDetails/NetworkingManagerUserDetailsResponseSuccessMock.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingManagerUserDetailsResponseSuccessMock.swift
+//  iOSTakeHomeProjectTests
+//
+//  Created by Ruslan Magomedov on 04.08.2023.
+//
+
+import Foundation
+@testable import iOSTakeHomeProject
+
+class NetworkingManagerUserDetailsResponseSuccessMock: NetworkingManagerImpl {
+    func request<T>(session: URLSession, _ endpoint: iOSTakeHomeProject.Endpoint, type: T.Type) async throws -> T where T : Decodable, T : Encodable {
+        return try StaticJSONMapper.decode(file: "SingleUserData", type: UserDetailResponse.self) as! T
+    }
+    
+    func request(session: URLSession, _ endpoint: iOSTakeHomeProject.Endpoint) async throws {  }
+}
